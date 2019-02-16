a8a621ac2895d2b9fb000629a6f5995b88e6471c
Eelco Okken <{ID}+{username}@users.noreply.github.com>
2019-02-16 13:12:28 +0100

    cmmnd

--- legendary-chainsaw.ino
+++ legendary-chainsaw.ino
@@ -4,6 +4,7 @@
 #endif
 
 #define PIN 2
+#define NUM_LEDS 97
 
 //Globals
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(97, PIN, NEO_GRB + NEO_KHZ800); //Params 1 - pixels; 2 - PIN; 3 - Flags (may be added(+) together) (NEO_KHZ800, NEO_KHZ400, NEO_GRB, NEO_RGB, NEO_RGBW)
@@ -30,20 +31,26 @@ public:
 	}
 	void SetPattern() {
 		for (int i = 0; i < strip.numPixels(); i++) {
-			strip.setPixelColor(Pos, 255, 255, 255, 255);
 			strip.setPixelColor(i, Wheel((i + Pos) & 255));
 		}
 	}
 };
+
 class Pong {
 private:
-	int Pos, InversedSpeed, Step, Size;
+	int Pos, InversedSpeed, Step, Size, Length;
+	uint8_t R, G, B;
 	bool Clockwise;
 public:
-	Pong(int invSpd, int size) {
+	Pong(int invSpd, int size, uint8_t r, uint8_t g, uint8_t b, bool clockwise, int startPos, int length ) {
 		Size = size;
 		InversedSpeed = invSpd;
-		Clockwise = true;
+		Clockwise = clockwise;
+		if(!Clockwise)
+			Pos = 95;
+		R = r; G = g; B = b;
+		Pos = startPos;
+		Length = length;
 	}
 	void Next(int Amount) {
 		Step += Amount;
@@ -54,21 +61,23 @@ public:
 			else
 				Pos--;
 		}
-		if (Pos <= 0 || Pos >= strip.numPixels())
+		if (Pos <= 0 || Pos >= Length)
 			Clockwise = !Clockwise;
 	}
 	void BlendPattern() {
 		for (int i = Pos - Size / 2; i <= Pos + Size / 2; i++)
 		{
-			if (i >= 0 && i < strip.numPixels())
-				strip.setPixelColor(i - 1, 255, 255, 255, 255);
+			if (i > 0 && i < Length)
+				strip.setPixelColor(i - 1, R, G, B, 255);
 		}
 	}
 };
 
 
 Rainbow r = Rainbow();
-Pong p = Pong(1,3);
+Pong p = Pong(1,3, 255,255,255, true, 1, NUM_LEDS);
+Pong q = Pong(1, 3, 255, 0, 0, false, 96, NUM_LEDS);
+
 int Offset;
 
 void setup() {
@@ -81,8 +90,10 @@ void loop() {
 	Offset++;
 	r.SetPattern();
 	p.BlendPattern();
+	q.BlendPattern();
 	r.Next(1);
 	p.Next(1);
+	q.Next(1);
 	strip.show();
-	delay(40);
+	delay(30);
 }
